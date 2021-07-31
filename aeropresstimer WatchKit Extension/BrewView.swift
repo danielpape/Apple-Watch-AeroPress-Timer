e344e9b56bda1d49b6ac4d59a34df6389b15d1fc
Daniel Pape <danielwpape@gmail.com>
2021-07-31 11:06:54 +0100

    Initial Commit

---
+++ aeropresstimer WatchKit Extension/BrewView.swift
@@ -0,0 +1,20 @@
+//
+//  BrewView.swift
+//  aeropresstimer WatchKit Extension
+//
+//  Created by Daniel Pape on 31/07/2021.
+//
+
+import SwiftUI
+
+struct BrewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BrewView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrewView()
+    }
+}
--- aeropresstimer WatchKit Extension/ContentView.swift
+++ aeropresstimer WatchKit Extension/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var brewMins: Int = 1
+    @State private var brewSecs: Int = 0
+
+//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+//    let duration = 20
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+            VStack{
+                HStack{
+                    Picker(selection: $brewMins, label: Text("Mins"), content: {
+                        Text("0").tag(0)
+                    Text("1").tag(1)
+                    Text("2").tag(2)
+                        Text("3").tag(4)
+                        Text("4").tag(4)
+                })
+                    Picker(selection: $brewSecs, label: Text("Secs"), content: {
+                    Text("1").tag(1)
+                    Text("2").tag(2)
+                })
+                }
+            Spacer()
+                NavigationLink(
+                    destination: BrewView2(),
+                    label: {
+                        Text("Begin")
+                    }
+            )
+        .navigationTitle("Brewsta")
+    }
+}
+}
+
+struct BrewView2:View{
+
+    @State private var length: Int = 20
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View{
+        Text("\(length)")
+            .navigationTitle("Brew")
+            .onReceive(timer) { time in
+                print("active")
+                length=length-1
+                
+                if(length==0){
+                    self.timer.upstream.connect().cancel()
+                }
+            }
     }
 }
 
