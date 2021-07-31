0b200b1f300adfefc14f92b22fe7e1b75d5000db
Daniel Pape <danielwpape@gmail.com>
2021-07-31 15:47:58 +0100

    alpha, needs to set stir time

--- aeropresstimer WatchKit Extension/ContentView.swift
+++ aeropresstimer WatchKit Extension/ContentView.swift
@@ -11,56 +11,83 @@ struct ContentView: View {
     
     @State private var brewMins: Int = 1
     @State private var brewSecs: Int = 0
-
-//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-//    let duration = 20
+    
+    //    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    //    let duration = 20
     
     var body: some View {
-            VStack{
-                HStack{
-                    Picker(selection: $brewMins, label: Text("Mins"), content: {
-                        Text("0").tag(0)
-                    Text("1").tag(1)
-                    Text("2").tag(2)
-                        Text("3").tag(4)
-                        Text("4").tag(4)
-                })
-                    Picker(selection: $brewSecs, label: Text("Secs"), content: {
-                    Text("1").tag(1)
-                    Text("2").tag(2)
-                })
-                }
-            Spacer()
-                NavigationLink(
-                    destination: BrewView2(),
-                    label: {
-                        Text("Begin")
+        VStack(alignment: .leading, spacing: 4.0){
+            Text("Set Brew Time")
+            HStack{
+                Picker(selection: $brewMins, label: Text("Minutes"), content: {
+                    ForEach(0..<11){min in
+                    Text("\(min)").tag(min)
                     }
+                })
+                Text(":")
+                Picker(selection: $brewSecs, label: Text("Seconds"), content: {
+                    ForEach(0..<60){sec in
+                    Text("\(sec)").tag(sec)
+                    }
+                })
+            }
+            Spacer()
+            NavigationLink(
+                destination: BrewView(count: brewMins*60+brewSecs, staticCount: brewMins*60+brewSecs),
+                label: {
+                    Text("Begin")
+                }
             )
-        .navigationTitle("Brewsta")
+            .background(Color.green)
+            .clipShape(Capsule()
+            )
+            .navigationTitle("Brewsta")
+        }
     }
 }
-}
 
-struct BrewView2:View{
-
-    @State private var length: Int = 20
+struct BrewView:View{
     
+    @State private var to: CGFloat = 1
+    @State var count = 150
+    
+    var staticCount = 0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View{
-        Text("\(length)")
-            .navigationTitle("Brew")
-            .onReceive(timer) { time in
-                print("active")
-                length=length-1
-                
-                if(length==0){
-                    self.timer.upstream.connect().cancel()
+
+        ZStack{
+            Circle()
+                .trim(from: 0, to: 1)
+                .stroke(Color.white.opacity(0.09),style: StrokeStyle(lineWidth: 16, lineCap: .round))
+            //                .frame(width: 280, height: 280, alignment: .center)
+            Circle()
+                .trim(from: 0, to: self.to)
+                .stroke(Color.green,style: StrokeStyle(lineWidth: 16, lineCap: .round))
+                .rotationEffect(.init(degrees: -90))
+            Text(count == 0 ? "Enjoy" : "\(count/60):\(String(format: "%02d", count%60))")
+                .navigationTitle("Brew")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .onReceive(timer) { time in
+                    print("active")
+                    count-=1
+                    WKInterfaceDevice.current().play(.click)
+                    withAnimation(.default){
+                        self.to = CGFloat(self.count)/CGFloat(staticCount)
+                    }
+                    
+                    if(count==30){
+                        WKInterfaceDevice.current().play(.start)
+                    }
+                    
+                    if(count==0){
+                        self.timer.upstream.connect().cancel()
+                    }
                 }
-            }
+        }.padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
